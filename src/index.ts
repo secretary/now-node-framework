6c365c96f64cf03920209b1df49bc9bede97b048
Aaron Scherer <aequasi@gmail.com>
2019-05-01 19:07:25 -0700

    Adding event dispatcher

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ export {default as ResponseInterface} from './Interface/ResponseInterface';
 export {default as Route} from './Decorator/Route';
 export {default as AbstractAction} from './Action/AbstractAction';
 export {default as ActionInterface} from './Action/ActionInterface';
+export {default as AbstractListener} from './Event/AbstractListener';
+export {default as Dispatcher} from './Event/Dispatcher';
 export {default as Event} from './Event/Event';
 export {default as HTTPEvent} from './Event/HTTPEvent';
 export {default as HTTPExceptionEvent} from './Event/HTTPExceptionEvent';
